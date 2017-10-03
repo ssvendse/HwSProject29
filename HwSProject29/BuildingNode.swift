d1b63886049e4c6fa6eb03bf57b15594b6f8f9b2
Skyler Svendsen <skylersvendsen@gmail.com>
2017-10-03 12:54:57 -0500

    first commit

---
+++ HwSProject29/BuildingNode.swift
@@ -0,0 +1,13 @@
+//
+//  BuildingNode.swift
+//  HwSProject29
+//
+//  Created by Skyler Svendsen on 10/3/17.
+//  Copyright © 2017 Skyler Svendsen. All rights reserved.
+//
+
+import UIKit
+
+class BuildingNode: SKSpriteNode {
+
+}
